9984810359cc49a3fa4b088bcbc19bd93ae9d8e2
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2023-09-13 23:18:20 +0700

    thuanvv/refactor-build-4

--- src/shared/components/customization/form/inputSelect.tsx
+++ src/shared/components/customization/form/inputSelect.tsx
@@ -18,7 +18,7 @@ type Props = {
     options?: { value: any, label: string }[]
 }
 
-export default function InputSelect({ className, form, label, placeHolder, fieldName, options = [] }: Props) {
+export function InputSelect({ className, form, label, placeHolder, fieldName, options = [] }: Props) {
     return (
         <FormField
             control={form.control}
