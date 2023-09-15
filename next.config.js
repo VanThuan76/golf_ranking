fe3a99fcf302b2b65364ee8c32a365e3f874cf01
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2023-09-15 12:15:45 +0700

    thuanvv/refactor-https-3

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
   reactStrictMode: true,
-  useFileSystemPublicRoutes: false,
   i18n,
   typescript: {
     tsconfigPath: './tsconfig.json',
